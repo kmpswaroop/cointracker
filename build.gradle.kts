9850fabb6a11b6a99f05efd4958856a2b42b2e49
kmpswaroop <pranava.swaroop@protonmail.com>
2025-07-07 17:44:04 +0530

    Adds gradle wrapper and updates gradle.build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "org.cointracker.transaction"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -14,6 +14,12 @@ dependencies {
     implementation("com.opencsv:opencsv:5.9")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.cointracker.transaction.Main"
+    }
 }
 
 tasks.test {
